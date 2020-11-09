99c7431d03e5e5a2546fe69c610dbdb8147523be
ivanopol <ivanopol777@mail.ru>
2020-11-09 16:53:18 +0500

    Добавил конфиг для pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'brightpark',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start',
+      env: {
+      	NODE_ENV: "production",
+      	PORT: 3001    
+      }
+    }
+  ]
+}
