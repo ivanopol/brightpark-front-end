e35818952d6df9d3bdf66dc10dbd434e79381c31
ivanopol <ivanopol777@mail.ru>
2020-11-18 18:36:17 +0500

    Добавил редирект для всех сбытовых страниц
    #redirects

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -1,9 +1,11 @@
 const redirects = require('../301.json')
 
 export default async ({ redirect, route }) => {
-  var route = route.path.slice(1)
-  var segments = route.split('/')
+
+  var route_str = route.path.slice(1)
+  var segments = route_str.split('/')
   var redirect_path = ''
+  var segments_tmp = {}
 
   var city = segments.shift()
 
@@ -11,11 +13,30 @@ export default async ({ redirect, route }) => {
     return {}
   }
 
-  route = '/' + segments.join('/')
+  if (segments[segments.length - 1] === 'model_details') {
+    segments_tmp = {
+      models: segments[0],
+      model:  segments[1]
+    }
 
-  const isRedirect = redirects.find(r => r.from === route)
+    segments[0] = '<models>'
+    segments[1] = '<model>'
+  }
+
+  route_str = '/' + segments.join('/')
+
+  var isRedirect = redirects.find(r => r.from === route_str)
 
   if (isRedirect) {
+
+    if (segments_tmp.hasOwnProperty('models')) {
+      isRedirect.to = isRedirect.to.replace('<models>', segments_tmp.models)
+    }
+
+    if (segments_tmp.hasOwnProperty('model')) {
+      isRedirect.to = isRedirect.to.replace('<model>', segments_tmp.model)
+    }
+
     redirect_path = '/' + city + isRedirect.to
     redirect(redirect_path)
   }
