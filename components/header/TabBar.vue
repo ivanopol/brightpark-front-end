bac74ede5154db153433349e1457caa20777cbd3
ivanopol <ivanopol777@mail.ru>
2020-11-24 15:39:14 +0500

    Изменил положение кнопки закрытия окна карты
    #tab-bar #map

--- components/header/TabBar.vue
+++ components/header/TabBar.vue
@@ -827,8 +827,8 @@ export default {
 
         .close {
             position: absolute;
-            top: 5px;
-            right: 5px;
+            top: 10px;
+            right: 10px;
             width: 20px;
             height: 20px;
             color: #FF8351;
