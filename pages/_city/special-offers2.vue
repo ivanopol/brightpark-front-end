8f26dcc193505a6dfc07a17615293e7f8386d4ee
FialDarg <53943035+FialDarg@users.noreply.github.com>
2020-12-03 17:00:11 +0500

    retargeting2 add

--- pages/_city/special-offers2.vue
+++ pages/_city/special-offers2.vue
@@ -43,9 +43,6 @@
           </li>
         </ul>
       </div>
-      <Steps :form_id="$store.state._page + '__fill-form_'"
-             :goal="'fixconditions'"
-             :prefix="$store.state._page + '__'" />
       <Baraban />
       <HookRetarget />
     </div>
