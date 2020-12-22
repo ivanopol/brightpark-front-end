21fbf7cd021a773713fc61b6e76af2493a5defbc
ivanopol <ivanopol777@mail.ru>
2020-12-22 18:25:51 +0500

    Включил снег #fix

--- components/elements/Snow.vue
+++ components/elements/Snow.vue
@@ -10,70 +10,69 @@ export default {
     if (!config.snow) {
       return false;
     }
-    document.addEventListener('DOMContentLoaded', function(){
-      var script = document.createElement('script');
-      script.src = 'https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js';
-      script.onload = function(){
-        particlesJS("snow", {
-          "particles": {
-            "number": {
-              "value": 100,
-              "density": {
-                "enable": true,
-                "value_area": 800
-              }
-            },
-            "color": {
-              "value": "#ffffff"
-            },
-            "opacity": {
-              "value": 0.7,
-              "random": false,
-              "anim": {
-                "enable": false
-              }
-            },
-            "size": {
-              "value": 3,
-              "random": true,
-              "anim": {
-                "enable": false
-              }
-            },
-            "line_linked": {
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js';
+    script.onload = function(){
+      particlesJS("snow", {
+        "particles": {
+          "number": {
+            "value": 100,
+            "density": {
+              "enable": true,
+              "value_area": 800
+            }
+          },
+          "color": {
+            "value": "#ffffff"
+          },
+          "opacity": {
+            "value": 0.7,
+            "random": false,
+            "anim": {
+              "enable": false
+            }
+          },
+          "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+              "enable": false
+            }
+          },
+          "line_linked": {
+            "enable": false
+          },
+          "move": {
+            "enable": true,
+            "speed": 2,
+            "direction": "bottom",
+            "random": true,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+              "enable": true,
+              "rotateX": 300,
+              "rotateY": 1200
+            }
+          }
+        },
+        "interactivity": {
+          "events": {
+            "onhover": {
               "enable": false
             },
-            "move": {
-              "enable": true,
-              "speed": 2,
-              "direction": "bottom",
-              "random": true,
-              "straight": false,
-              "out_mode": "out",
-              "bounce": false,
-              "attract": {
-                "enable": true,
-                "rotateX": 300,
-                "rotateY": 1200
-              }
-            }
-          },
-          "interactivity": {
-            "events": {
-              "onhover": {
-                "enable": false
-              },
-              "onclick": {
-                "enable": false
-              },
-              "resize": false
-            }
-          },
-          "retina_detect": true
-        });
-      }
-      document.head.append(script);
-    });
+            "onclick": {
+              "enable": false
+            },
+            "resize": false
+          }
+        },
+        "retina_detect": true
+      });
+    }
+    document.head.append(script);
+
   }
 }
 </script>
