64c0f8ddf82c81c798fc1573fc479c6caf527ddf
ivanopol <ivanopol777@mail.ru>
2021-01-15 17:39:55 +0500

    Добавил счётчик RoiStat #upd
    #scripts #roistat

--- components/Scripts.vue
+++ components/Scripts.vue
@@ -218,12 +218,7 @@ export default {
     roiStat(tag_body) {
       var roiStat = document.createElement("script")
       roiStat.className = 'loading_scripts'
-      roiStat.innerHTML = '(function(w, d, s, h, id) {\n' +
-        '    w.roistatProjectId = id; w.roistatHost = h;\n' +
-        '    var p = d.location.protocol == "https:" ? "https://" : "http://";\n' +
-        '    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);\n' +
-        '    var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);\n' +
-        '})(window, document, \'script\', \'cloud.roistat.com\', \'1b4950a1a14d1899c2e2a4c9bb8ea4d0\');'
+      roiStat.innerHTML = '(function(w, d, s, h, id) {    w.roistatProjectId = id; w.roistatHost = h;    var p = d.location.protocol == "https:" ? "https://" : "http://";    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);    var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);})(window, document, \'script\', \'cloud.roistat.com\', \'1b4950a1a14d1899c2e2a4c9bb8ea4d0\');'
       tag_body.appendChild(roiStat)
     },
     googleAnalytics(tag_body) {
