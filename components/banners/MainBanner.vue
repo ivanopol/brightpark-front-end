b2cefe6dfe39da3b3bf48dfdd53dc8586e9b1aba
ivanopol <ivanopol777@mail.ru>
2021-05-31 14:28:10 +0500

    Убрал cssMode для декстопа
    #banner #main

--- components/banners/MainBanner.vue
+++ components/banners/MainBanner.vue
@@ -79,11 +79,6 @@ export default {
           el: '.swiper-pagination',
           clickable: true,
         },
-        breakpoints: {
-          581: {
-            cssMode:true
-          }
-        }
       }
     };
   },
