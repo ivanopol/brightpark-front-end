26e9ad009d8cea43afdf4cc8f9d41e98875f98f3
ivanopol <ivanopol777@mail.ru>
2021-06-09 20:33:24 +0500

    Добавил открытие окна календаря по нажатию
    #test-drive #form

--- components/forms/FormTestDriveOrder.vue
+++ components/forms/FormTestDriveOrder.vue
@@ -28,6 +28,7 @@
 
       <div class="test-drive__form__row">
         <v-date-picker
+          :popover="{ visibility: 'click' }"
           :min-date='new Date()'
           v-model="fields.date"
           :input-debounce="500"
@@ -40,12 +41,7 @@
               :value="inputValue"
               v-on="inputEvents"
             />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
               <path
                 d="M20 20h-4v-4h4v4zm-6-10h-4v4h4v-4zm6 0h-4v4h4v-4zm-12 6h-4v4h4v-4zm6 0h-4v4h4v-4zm-6-6h-4v4h4v-4zm16-8v22h-24v-22h3v1c0 1.103.897 2 2 2s2-.897 2-2v-1h10v1c0 1.103.897 2 2 2s2-.897 2-2v-1h3zm-2 6h-20v14h20v-14zm-2-7c0-.552-.447-1-1-1s-1 .448-1 1v2c0 .552.447 1 1 1s1-.448 1-1v-2zm-14 2c0 .552-.447 1-1 1s-1-.448-1-1v-2c0-.552.447-1 1-1s1 .448 1 1v2z"
               />
