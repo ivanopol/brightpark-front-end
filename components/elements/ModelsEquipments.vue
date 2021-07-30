cd32d1fb435acec8c73657cafd67b0c44d60b90b
nikita <fallgetell@gmail.com>
2021-07-30 15:18:42 +0500

    Добавил кнопку

--- components/elements/ModelsEquipments.vue
+++ components/elements/ModelsEquipments.vue
@@ -300,12 +300,26 @@
       </div>
     </div>
   </div>
+
+  <button type="button" class="equipments__button">
+    <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M10.1667 1C9.33333 1 8.77778 1.83333 8.77778 3.22222C8.77778 7.94444 14.3333 16 19.3333 16C20.1667 16 21 15.4444 21 14.6111C21 13.7778 20.1667 12.9444 17.9444 12.9444C12.6667 12.9444 1 16.5556 1 19.6111C1 20.1667 1.55556 21 2.66667 21C6 21 11.5556 10.1667 11.5556 3.22222C11.5556 2.11111 11 1 10.1667 1Z" stroke="white" stroke-width="1.5"/>
+    </svg>
+
+    Скачать прайс-лист
+  </button>
 </section>
 </template>
 
 <script>
 export default {
-  name: "ModelsEquipments"
+  name: "ModelsEquipments",
+
+  data: function () {
+    return {
+
+    }
+  }
 }
 </script>
 
@@ -390,5 +404,27 @@ export default {
   .equipments__items__item__option__button {
     font-size: 11px;
     height: 100%;
+  }
+
+  .equipments__button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 14px;
+    text-transform: uppercase;
+    color: white;
+    padding: 10px 15px;
+    background: #5CBE86;
+    border: none;
+    width: 100%;
+    font-family: 'Factor A';
+    letter-spacing: 0.05em;
+    border-radius: 5px;
+    cursor: pointer;
+    height: 65px;
+
+    svg {
+      margin-right: 6px;
+    }
   }
 </style>
