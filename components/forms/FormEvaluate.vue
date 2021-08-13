21f860ab9bb788c702893fdd25f6633933d5975a
nikita <fallgetell@gmail.com>
2021-08-13 14:50:18 +0500

    Добавил форму с оценкой

---
+++ components/forms/FormEvaluate.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="evaluate">
+  <h4 class="evaluate__title">
+    Оценка вашего автомобиля
+  </h4>
+
+  <div class="evaluate__car-info">
+    <div class="evaluate__car-info__item">
+
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "FormEvaluate"
+}
+</script>
+
+<style scoped>
+
+</style>
