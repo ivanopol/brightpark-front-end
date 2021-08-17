18962f481fbafb8089d6f0d603ea14f0eacb4f0c
ivanopol <ivanopol777@mail.ru>
2021-08-17 16:22:56 +0500

    Добавил подстановку модели в блок тест-драйва
    #models #test-drive

--- components/elements/TestDriveBanner.vue
+++ components/elements/TestDriveBanner.vue
@@ -6,13 +6,8 @@
           <img src="~static/images/models/da.png" alt="" class="test-drive-banner__pic">
 
           <div class="test-drive-banner__text">
-            <h2>
-              Испытай новую LADA Granta в деле!
-            </h2>
-
-            <p>
-              Ощутите преимущества новой LADA Granta Седан на собственном опыте
-            </p>
+            <h2>Испытай новую LADA Granta в деле!</h2>
+            <p>Ощутите преимущества новой LADA {{this.$store.state.car.model_full}} на собственном опыте</p>
           </div>
 
           <ButtonNew
