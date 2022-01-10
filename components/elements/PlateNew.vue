d0652551ee83cf89c263762ca5a0a84dfe8b9f89
ivanopol <ivanopol777@mail.ru>
2022-01-10 11:29:23 +0500

    Вернул надпись на плашке
    #plate #main

--- components/elements/PlateNew.vue
+++ components/elements/PlateNew.vue
@@ -2,7 +2,7 @@
   <div class="container-big plate-new" v-observe-visibility="{ callback: visibilityChanged, intersection: { threshold: 0 }, }">
     <transition-group name="slide-fade" tag="div" v-on:enter="enter">
       <div v-bind:key="1" :class="{'slide-fade-enter-active' : show, 'slide-fade-leave-active slide-fade-leave-to' : !show}">
-        <p>31 декабря салоны работают до 14:00, 1 января - выходной день, со 2 января работаем в обычном режиме.</p>
+        <p>Все дилерские центры Брайт Парк продолжают работать и&nbsp;продавать LADA, с&nbsp;соблюдением всех нормативных требований и&nbsp;правил безопасности</p>
       </div>
     </transition-group>
 <!--    <div class="plate-new__button-wrap">
