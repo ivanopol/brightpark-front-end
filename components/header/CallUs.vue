749c06d90d3729cbca7a85aa76397587999ba479
ivanopol <ivanopol777@mail.ru>
2022-05-27 17:43:59 +0500

    Добавил подмену номера Callibri в выпадающую шапку
    #service

--- components/header/CallUs.vue
+++ components/header/CallUs.vue
@@ -16,9 +16,9 @@
            class="call-us__list__item event"
            v-bind:class="phone.replacement"
         >
-          <p class="call-us__list__item__title" v-if="phone.replacement !== 'callibri_phone_service_header'">{{ phone.title }}</p>
+          <p class="call-us__list__item__title" >{{ phone.title }}</p>
 
-          <span v-if="phone.replacement !== 'callibri_phone_service_header'">
+          <span>
             <img :src="'/images/icons/header/' + phone.icon + '.svg'"
                  :alt="phone.title"
                  class="call-us__list__item__icon"
