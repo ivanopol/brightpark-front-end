786356115e2ffa1d0fd18796a169c807c90df7c9
ivanopol <ivanopol777@mail.ru>
2022-06-29 16:37:09 +0500

    Добавил формы для пролонгации и сервиса

---
+++ pages/_city/special-sale.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="theme-05-special">
+    <FormLeadNew
+      class='special-form'
+      form_title='Оставить заявку'
+      :needCity='true'
+      goal="viber_mailing"
+      form_title_special='Заявка с вайбера/продажи'
+      :form_type="7"
+    />
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import "./assets/scss/_colors.scss";
+
+.special-form {
+  color: $purple;
+  margin: auto;
+}
+</style>
